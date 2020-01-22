3ca08e977bb849649a5a93594c379a7dc72993fd
ledeni56 <lovro.ludvig@gmail.com>
2020-01-22 14:19:49 +0100

    improving DataLoader.py

--- code/DataLoader.py
+++ code/DataLoader.py
@@ -3,66 +3,50 @@ import numpy as np
 import os
 from configure import Config
 
-config = Config()
+class DataLoaderHelper:
+    @staticmethod
+    def path_conj(pathname):
+        pathtrain = pathname
+        filestrain = os.listdir(pathtrain)
+        train_list_file = [os.path.join(pathtrain, f) for f in filestrain]
+        file_list = train_list_file
+        return file_list
 
-def main():
+    @staticmethod
+    def load_images(file_list):
+        return_list = []
+        for file_name in file_list:
+            with Image.open(file_name) as image:
+                return_list.append(np.array(image) / 255)
+        return return_list
 
-        face1 = []
-        face2 = []
-        face3 = []
-        face4 = []
-        face5 = []
-        face = [face1, face2, face3, face4, face5]
-        unknown_faces = []
-        all_faces = []
-        background = []
-        paths = []
+    @staticmethod
+    def getImagePaths(mode):
+        return ["../lfwcrop_grey/izabrana_lica/" + mode + "/lice1/",
+            "../lfwcrop_grey/izabrana_lica/" + mode + "/lice2/",
+            "../lfwcrop_grey/izabrana_lica/" + mode + "/lice3/",
+            "../lfwcrop_grey/izabrana_lica/" + mode + "/lice4/",
+            "../lfwcrop_grey/izabrana_lica/" + mode + "/lice5/",
+            "../lfwcrop_grey/izabrana_lica/" + mode + "/unknown_lica/",
+            "../lfwcrop_grey/izabrana_lica/" + mode + "/resized background 64x64/"]
 
-        if config.mode == "training":
-            mode = "training"
-        else:
-            mode = "testing"
+class DataLoader:
+    def __init__(self, mode):
+        self.images = [[], [], [], [], [], [], []]
+        self.all_faces = []
+        self.mode = mode
 
-        print("Starting to work...")
+    def load_all_images(self):
+        print("started loading...")
+        paths = DataLoaderHelper.getImagePaths(self.mode)
+        for i in range(len(paths)):
+            file_list = DataLoaderHelper.path_conj(paths[i])
+            self.images[i] = DataLoaderHelper.load_images(file_list)
 
-        # defining the path and directory with images
-        # Face 1 - 5
-        paths.append("../lfwcrop_grey/izabrana_lica/" + mode + "/lice1/")
-        paths.append("../lfwcrop_grey/izabrana_lica/" + mode + "/lice2/")
-        paths.append("../lfwcrop_grey/izabrana_lica/" + mode + "/lice3/")
-        paths.append("../lfwcrop_grey/izabrana_lica/" + mode + "/lice4/")
-        paths.append("../lfwcrop_grey/izabrana_lica/" + mode + "/lice5/")
+        self.all_faces = np.concatenate((self.images[0], self.images[1], self.images[2], self.images[3], self.images[4], self.images[5]))
+        print(len(self.images[0]), len(self.all_faces), len(self.images[6]))
+        print("finished loading.")
 
-        for i in paths:
-            print(i)
-            file_list = path_conj(i)
-            load_images(file_list, face[paths.index(i)])
-
-        # Unknown faces
-        pathtrain = "../lfwcrop_grey/izabrana_lica/" + mode + "/unknown_lica/"
-        file_list = path_conj(pathtrain)
-        load_images(file_list, unknown_faces)
-
-        # Background
-        pathtrain = "../lfwcrop_grey/izabrana_lica/" + mode + "/resized background 64x64/"
-        file_list = path_conj(pathtrain)
-        load_images(file_list, background)
-
-        # All faces
-        all_faces = np.concatenate((face1, face2, face3, face4, face5, unknown_faces))
-        print(len(face1), len(unknown_faces), len(all_faces), len(background))
-
-def path_conj(pathname):
-    pathtrain = pathname
-    filestrain = os.listdir(pathtrain)
-    train_list_file = [os.path.join(pathtrain, f) for f in filestrain]
-    file_list = train_list_file
-    return file_list
-
-def load_images(file_list, face_list):
-    for file_name in file_list:
-        with Image.open(file_name) as image:
-            face_list.append(np.array(image) / 255)
-    return face_list
-
-main()
+#USE THIS TO TEST:
+#dl = DataLoader(Config().mode)
+#dl.load_all_images()
