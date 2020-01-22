71c7115812c680358344549f75f6d2f390443ea1
mcolja <colja.martin@gmail.com>
2020-01-22 20:52:00 +0100

    optimized PCA calculations

--- code/PCA.py
+++ code/PCA.py
@@ -1,24 +1,11 @@
 import numpy as np
 
-class PCA():
+class FeatureExtractor():
 
-    def __init__(self, number_of_components, list_of_face_matrices):
-        print("PCA initialization started")
-        faces_matrix = np.vstack([face_matrix.flatten() for face_matrix in list_of_face_matrices])
-        self.average_face = np.sum(faces_matrix, axis = 0)/np.size(faces_matrix, axis = 0)
-        diffrences_matrix = np.vstack([face - self.average_face for face in faces_matrix])
-        covarience_matrix = diffrences_matrix.transpose() @ diffrences_matrix
-        transform_matrix = PCA._generate_transform_matrix(number_of_components, covarience_matrix)
-        self._transform_matrix = np.transpose(transform_matrix)
-        print("PCA initialization done")
+    def __init__(self, eigenfaces_url, average_face_url):
+        self.eigenfaces = np.loadtxt(eigenfaces_url, delimiter=',')
+        self.average_face = np.loadtxt(average_face_url, delimiter=',')
 
-    @staticmethod
-    def _generate_transform_matrix(number_of_components, covarience_matrix):
-        #TODO: Remove complex values from eigenvalues and eigenvectors
-        e_vals, e_vecs = np.linalg.eig(covarience_matrix)
-        return np.real(e_vecs[0:number_of_components, :])
-
-    # generates dataset with number_of_component features and class of image
     def generate_dataset(self, images):
         dataset = []
         for data_class in range(len(images)):
@@ -29,11 +16,31 @@ class PCA():
         return np.vstack(dataset)
 
     def extract_features(self, image_matrix, **kwargs):
-        features = (image_matrix.flatten() - self.average_face) @ self._transform_matrix
+        features = (image_matrix.flatten() - self.average_face) @ self.eigenfaces
         if kwargs['transposed'] == True:
             return np.transpose(features)
         return features
 
+def extract_eigenfaces(number_of_eigenfaces, list_of_face_matrices):
+    faces_matrix = np.vstack([face_matrix.flatten() for face_matrix in list_of_face_matrices])
+    average_face = np.sum(faces_matrix, axis = 0)/np.size(faces_matrix, axis = 0)
+    diffrences_matrix = np.vstack([face - average_face for face in faces_matrix])
+    covarience_matrix = diffrences_matrix.transpose() @ diffrences_matrix
+    transform_matrix = generate_transform_matrix(number_of_eigenfaces, covarience_matrix)
+    return np.transpose(transform_matrix), average_face
+
+def generate_transform_matrix(number_of_components, covarience_matrix):
+    e_vals, e_vecs = np.linalg.eig(covarience_matrix)
+    return np.real(e_vecs[0:number_of_components, :])
+
 if __name__ == "__main__":
-    print("No functionality yet")
-    pass
+    from DataLoader import DataLoader
+    from configure import Config
+    config = Config()
+    dl = DataLoader(config.modeTrain)
+    dl.load_all_images()
+    print("PCA init start")
+    eigenfaces, average_face = extract_eigenfaces(config.noOfEigenValues, dl.all_faces)
+    print("PCA end")
+    np.savetxt('eigenfaces.csv', eigenfaces, delimiter=',')
+    np.savetxt('average_face.csv', average_face, delimiter=',')
--- code/mainscript.py
+++ code/mainscript.py
@@ -1,5 +1,5 @@
 from NeuralNetwork import NeuralNetwork
-from PCA import PCA
+from PCA import FeatureExtractor
 from configure import Config
 import numpy as np
 from DataLoader import DataLoader
@@ -9,14 +9,14 @@ if __name__ == "__main__":
     config = Config()
     dl = DataLoader(config.modeTrain)
     dl.load_all_images()
-    pca = PCA(config.noOfEigenValues, dl.all_faces)
-    dataset = pca.generate_dataset(dl.images)
+    fe = FeatureExtractor("eigenfaces.csv", "average_face.csv")
+    dataset = fe.generate_dataset(dl.images)
     nn = NeuralNetwork(config.noOfEigenValues, config.noOfHidNeur)
     nn.trainNetwork(dataset)
 
     dl = DataLoader(config.modeTest)
     dl.load_all_images()
-    dataset = pca.generate_dataset(dl.images)
+    dataset = fe.generate_dataset(dl.images)
     
     print(nn.classify(np.transpose(np.asmatrix(dataset[0][:-1]))))
     print(nn.classify(np.transpose(np.asmatrix(dataset[1][:-1]))))
