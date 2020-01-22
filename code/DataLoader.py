1b11e9aec16136ac14958a9bcdb2b8453112944d
Ardita Salja <ardita.salja@gmail.com>
2020-01-22 12:25:32 +0100

    dataloader i configure

--- code/DataLoader.py
+++ code/DataLoader.py
@@ -1,6 +1,9 @@
 from PIL import Image
 import numpy as np
 import os
+from configure import Config
+
+config = Config()
 
 def main():
 
@@ -13,90 +16,64 @@ def main():
         all_faces = []
         background = []
 
+        if config.mode == "training":
+            mode = "training"
+        else:
+            mode = "testing"
+
         print("Starting to work...")
 
         # defining the path and directory with images
         # Face 1
-        pathtrain = "../lfwcrop_grey/izabrana_lica/lice1/"
-        filestrain = os.listdir(pathtrain)
-        train_list_file = [os.path.join(pathtrain, f) for f in filestrain]
-        file_list = train_list_file
-
-        # loading the images into a list of matrices
-        for file_name in file_list:
-            with Image.open(file_name) as image:
-                face1.append(np.array(image)/255)
+        pathtrain = "../lfwcrop_grey/izabrana_lica/" + mode + "/lice1/"
+        file_list = path_conj(pathtrain)
+        load_images(file_list, face1)
 
         # Face 2
-        pathtrain = "../lfwcrop_grey/izabrana_lica/lice2/"
-        filestrain = os.listdir(pathtrain)
-        train_list_file = [os.path.join(pathtrain, f) for f in filestrain]
-        file_list = train_list_file
-
-        # loading the images into a list of matrices
-        for file_name in file_list:
-            with Image.open(file_name) as image:
-                face2.append(np.array(image) / 255)
+        pathtrain = "../lfwcrop_grey/izabrana_lica/" + mode + "/lice2/"
+        file_list = path_conj(pathtrain)
+        load_images(file_list, face2)
 
         #Face 3
-        pathtrain = "../lfwcrop_grey/izabrana_lica/lice3/"
-        filestrain = os.listdir(pathtrain)
-        train_list_file = [os.path.join(pathtrain, f) for f in filestrain]
-        file_list = train_list_file
+        pathtrain = "../lfwcrop_grey/izabrana_lica/" + mode + "/lice3/"
+        file_list = path_conj(pathtrain)
+        load_images(file_list, face3)
 
-        # loading the images into a list of matrices
-        for file_name in file_list:
-            with Image.open(file_name) as image:
-                face3.append(np.array(image) / 255)
         #Face 4
-        pathtrain = "../lfwcrop_grey/izabrana_lica/lice4/"
-        filestrain = os.listdir(pathtrain)
-        train_list_file = [os.path.join(pathtrain, f) for f in filestrain]
-        file_list = train_list_file
-
-        # loading the images into a list of matrices
-        for file_name in file_list:
-            with Image.open(file_name) as image:
-                face4.append(np.array(image) / 255)
+        pathtrain = "../lfwcrop_grey/izabrana_lica/" + mode + "/lice4/"
+        file_list = path_conj(pathtrain)
+        load_images(file_list, face4)
 
         # Face 5
-        pathtrain = "../lfwcrop_grey/izabrana_lica/lice5/"
-        filestrain = os.listdir(pathtrain)
-        train_list_file = [os.path.join(pathtrain, f) for f in filestrain]
-        file_list = train_list_file
-
-        # loading the images into a list of matrices
-        for file_name in file_list:
-            with Image.open(file_name) as image:
-                face5.append(np.array(image) / 255)
+        pathtrain = "../lfwcrop_grey/izabrana_lica/" + mode + "/lice5/"
+        file_list = path_conj(pathtrain)
+        load_images(file_list, face5)
 
         # Unknown faces
-        pathtrain = "../lfwcrop_grey/izabrana_lica/unknown_lica/"
-        filestrain = os.listdir(pathtrain)
-        train_list_file = [os.path.join(pathtrain, f) for f in filestrain]
-        file_list = train_list_file
-
-        # loading the images into a list of matrices
-        for file_name in file_list:
-            with Image.open(file_name) as image:
-                unknown_faces.append(np.array(image) / 255)
+        pathtrain = "../lfwcrop_grey/izabrana_lica/" + mode + "/unknown_lica/"
+        file_list = path_conj(pathtrain)
+        load_images(file_list, unknown_faces)
 
         # Background
-        pathtrain = "../lfwcrop_grey/izabrana_lica/resized background 64x64/"
-        filestrain = os.listdir(pathtrain)
-        train_list_file = [os.path.join(pathtrain, f) for f in filestrain]
-        file_list = train_list_file
-
-        # loading the images into a list of matrices
-        for file_name in file_list:
-            with Image.open(file_name) as image:
-                background.append(np.array(image) / 255)
+        pathtrain = "../lfwcrop_grey/izabrana_lica/" + mode + "/resized background 64x64/"
+        file_list = path_conj(pathtrain)
+        load_images(file_list, background)
 
         # All faces
-        all_faces = np.concatenate((face1, face2, face3, face4, face5))
-        print(len(all_faces))
-        print(len(background))
-        #max_el = np.amax (raw_data)
-        #min_el = np.amin(raw_data)
-        #print("(max el, min el): ", max_el, min_el)
+        all_faces = np.concatenate((face1, face2, face3, face4, face5, unknown_faces))
+        print(len(face1), len(all_faces), len(background))
+
+def path_conj(pathname):
+    pathtrain = pathname
+    filestrain = os.listdir(pathtrain)
+    train_list_file = [os.path.join(pathtrain, f) for f in filestrain]
+    file_list = train_list_file
+    return file_list
+
+def load_images(file_list, face_list):
+    for file_name in file_list:
+        with Image.open(file_name) as image:
+            face_list.append(np.array(image) / 255)
+    return face_list
+
 main()
