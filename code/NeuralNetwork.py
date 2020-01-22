cca80ed4bc32a82c1832b0aea19becbf5bb3cbd0
ledeni56 <lovro.ludvig@gmail.com>
2020-01-22 13:25:49 +0100

    nn WIP

---
+++ code/NeuralNetwork.py
@@ -0,0 +1,39 @@
+import numpy as np
+from configure import Config
+
+class SigmoidFunction:
+    @staticmethod
+    def calcSig(x):
+        return 1/(1 + np.exp(-x))
+
+    @staticmethod
+    def calcSigDer(x):
+        return SigmoidFunction.calcSig(x)*(1-SigmoidFunction.calcSig(x))
+
+class LayerOutputCalculator:
+    def __init__(self, weights, input):
+        self.outputNonActivated = weights*input
+        self.outputActivated = SigmoidFunction.calcSig(self.outputNonActivated)
+
+class NeuralNetwork:
+    def __init__(self, inputLayer, hiddenLayer):
+        #Xavier initialization
+        self.config = Config()
+        self.inputLayer = inputLayer
+        self.hiddenLayer = np.random.randn(hiddenLayer,inputLayer)*np.sqrt(1/inputLayer)
+        self.outputLayer = np.random.randn(self.config.noOfNNOutputs,hiddenLayer)*np.sqrt(1/hiddenLayer)
+
+    def classify(self, input):
+        hiddenLayerOutput = LayerOutputCalculator(self.hiddenLayer, input)
+        outputLayerOutput = LayerOutputCalculator(self.outputLayer, hiddenLayerOutput.outputActivated)
+        activatedOutputs = outputLayerOutput.outputActivated
+        return outputLayerOutput
+
+
+
+nn = NeuralNetwork(2,5)
+output = nn.classify(np.matrix('1;0'))
+print(nn.hiddenLayer)
+print(nn.outputLayer)
+print(output.outputActivated)
+print(np.where(output.outputActivated == max(output.outputActivated)))
--- code/configure.py
+++ code/configure.py
@@ -1,3 +1,4 @@
 class Config:
     def __init__(self):
         self.mode = "testing"
+        self.noOfNNOutputs = 7
